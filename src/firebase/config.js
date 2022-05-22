fa5b1f57ec8c9d471c24d751acf8058636a25377
JoyKhweis <68160623+Jkhweis@users.noreply.github.com>
2022-05-21 23:34:41 -0500

    Getting Documents

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCizVQyWnKGS7DXnzQ59tq6cXdvRp2VzzA',
+  authDomain: 'readinglistapp-5f84a.firebaseapp.com',
+  projectId: 'readinglistapp-5f84a',
+  storageBucket: 'readinglistapp-5f84a.appspot.com',
+  messagingSenderId: '662620223520',
+  appId: '1:662620223520:web:784defb83ca443db502445',
+};
+
+// init firebase
+initializeApp(firebaseConfig);
+
+// init firestore
+const db = getFirestore();
+
+export { db };
